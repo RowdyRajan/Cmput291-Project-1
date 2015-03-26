2387b609b99e72175d693659024158817481612f
RowdyRajan <rajanram@hotmail.com>
2015-03-25 19:39:23 -0600

    Finished grabing data from the user and checking it

--- newVehicleRegistration.py
+++ newVehicleRegistration.py
@@ -1,36 +1,48 @@
-import helpers
+from helpers import *
 
 def main():
 	while(True):
-		serial_no = input("Enter new vehicle ID: ");
-		serial_no = askLoop(serial_no, "Invalid Vehicle ID. Please enter a new ID: ", VINisIN);
-		maker = input("Enter vehicle maker: ");
-		model = input("Enter vehicle model: ");
-		
-		year = input("Enter vehicle year: ");
-		year = digitChecker(year, "Invalid year. Please enter again: ")
-			
-		color = input("Enter vehicle color: ");
+		serial_no = input("Enter new vehicle ID: ")
+		serial_no = askLoop(serial_no, "Vehicle ID already exits. Please enter a new ID: ", VINisIn)
 	
+		maker = input("Enter vehicle maker: ")
+		maker = blackSpaceLoop(maker, "Invalid Maker. Please re-enter: ")
 
+		model = input("Enter vehicle model: ")
+		model = blackSpaceLoop(model, "Invalid Model. Please re-enter: ")
+		
+		year = input("Enter vehicle year: ")
+		year = maxWidthDigitChecker(year, 'Invalid year. Please enter again: ',4)		
+		color = input("Enter vehicle color: ")
+		color = blackSpaceLoop(maker, "Invalid Color. Please re-enter: ")
 		type_id = input("Enter the vehicle type_id: ")
 		#TODO: Write typeIDExists 
 		#type_id = askLoop(type_id, "Invalid type_id. Please enter again: ", typeIDExits);
-
+		
 	
 def askLoop(answer,askString, confirmFunction):
-	while(not answer.isdigit() or not confirmFunction(answer)):
-		answer = input(askString);
-
+	while(not answer.isdigit() or confirmFunction(answer)):
+		if not answer.isdigit():
+			answer = input("Not a valid number. Please re-enter:")
+		else:
+			answer = input(askString);
 	return answer;
 
 
-def digitChecker(answer, askString, maxNumberSize):
-	while(not answer.isdigit()	and len(answer) != maxNumberSize):
+def maxWidthDigitChecker(answer, askString, maxNumberSize):
+	while(not answer.isdigit()	or len(answer) != maxNumberSize):
 		answer = input(askString)
 	return int(answer)
  
 def digitChecker(answer, askString):
 	while(not answer.isdigit()):
 		answer = input(askString)
-	return int(answer) 
+	return int(answer)
+
+def blankSpaceLoop(answer, askString){
+	while answer.isspace():
+		answer = input(askString)
+	return answer
+} 
+if __name__ == "__main__":
+	main() 
