a417f323ebd8e6634a1c3573e383e4d35a93fb91
codingHappiness <mstrong@ualberta.ca>
2015-03-27 17:37:33 -0600

    cleaned.

--- helpers.py
+++ helpers.py
@@ -62,56 +62,6 @@ def getName(SIN):
 	statement = "select p.name from people p where (p.sin) = ('%s')" % (SIN)
 	return(ReturnData(statement))
 		
-def getHeight(SIN):
-	# Returns SIN's height
-	statement = "select p.height from people p where (p.sin) = ('%s')" % (SIN)
-	return(ReturnData(statement))
-		
-def getWeight(SIN):
-	# Returns SIN's weight
-	statement = "select p.weight from people p where (p.sin) = ('%s')" % (SIN)
-	return(ReturnData(statement))
-
-def getEyeColor(SIN):
-	# Returns  at SIN's eye Color
-	statement = "select p.eyecolor from people p where (p.sin) = ('%s')" % (SIN)
-	return(ReturnData(statement))
-
-def getHairColor(SIN):
-	# Returns SIN's hair Color
-	statement = "select p.haircolor from people p where (p.sin) = ('%s')" % (SIN)
-	return(ReturnData(statement))
-
-def getAddress(SIN):
-	# Returns SIN's address
-	statement = "select p.addr from people p where (p.sin) = ('%s')" % (SIN)
-	return(ReturnData(statement))
-		
-def getGender(SIN):
-	# Returns SIN's gender
-	statement = "select p.gender from people p where (p.sin) = ('%s')" % (SIN)
-	return(ReturnData(statement))
-
-def getBirthday(SIN):
-	# Returns SIN's birthday
-	statement = "select p.birthday from people p where (p.sin) = ('%s')" % (SIN)
-	return(ReturnData(statement))
-		
-def getVehicleModel(VIN):
-	# Returns the model of vehicle at VIN
-	statement = "SELECT v.model from vehicle v where (v.serial_no) = ('%s')" % (VIN)
-	return(ReturnData(statement))
-
-def getVehicleYear(VIN):
-	# Returns the year of the vehicle at VIN
-	statement = "SELECT v.year from vehicle v where (v.serial_no) = ('%s')" % (VIN)
-	return(ReturnData(statement))
-
-def getVehicleColor(VIN):
-	# Returns the color of the vehicle at VIN
-	statement = "SELECT v.color from vehicle v where (v.serial_no) = ('%s')" % (VIN)
-	return(ReturnData(statement))
-		
 def ticketIDinDB(input):
 	statement = "select t.ticket_no from ticket t where (t.ticket_no) = ('%s')" % idNum
 	return(ReturnData(statement))
